de1fd372cde8da6efbb07f93c36d66c92f27f614
denyslavrentiev <denisoddessa@gmail.com>
2024-12-07 15:00:22 +0100

    Initial commit

--- auth_service/app/__init__.py
+++ auth_service/app/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     handlers=[
-        logging.FileHandler("service.log"),
+        logging.FileHandler("auth_service.log"),
         logging.StreamHandler()
     ]
 )
