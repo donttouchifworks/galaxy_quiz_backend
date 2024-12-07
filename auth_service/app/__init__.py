fa4a6d86ef10580c376dc783f8cd8a6b9ba331aa
denyslavrentiev <denisoddessa@gmail.com>
2024-12-07 19:38:39 +0100

    scalability tests and some issues fixed

--- auth_service/app/__init__.py
+++ auth_service/app/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     handlers=[
-        logging.FileHandler("auth_service.log"),
+        logging.FileHandler("service.log"),
         logging.StreamHandler()
     ]
 )
